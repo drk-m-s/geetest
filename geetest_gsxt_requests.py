cfe3c827d65431884bad87bbec4ea3079050c741
50 <john.jianghan@gmail.com>
2017-07-26 14:45:44 +0800

    Add files via upload

--- geetest_gsxt_requests.py
+++ geetest_gsxt_requests.py
@@ -10,6 +10,28 @@ from bs4 import BeautifulSoup
 from PIL import Image
 import StringIO
 import datetime
+import urllib
+# import scrapy
+
+# encodeURIComponent(str)  |  urllib.quote(str, safe='~()*!.\'')
+
+proxyHost = "proxy.abuyun.com"
+proxyPort = "9010"
+
+proxyUser = ""
+proxyPass = ""
+
+proxyMeta = "http://%(user)s:%(pass)s@%(host)s:%(port)s" % {
+    "host" : proxyHost,
+    "port" : proxyPort,
+    "user" : proxyUser,
+    "pass" : proxyPass,
+}
+
+proxies = {
+    "http"  : proxyMeta,
+    "https" : proxyMeta,
+}
 
 class gsxt(object):
     def __init__(self, tsb):
@@ -18,8 +40,8 @@ class gsxt(object):
         self.ck_gsxt = {}
         self.ck_geetest = {}
         self.info = {}
-        self.user_agent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko)  Safari/537.36"
-
+        self.user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+        self.proxies = proxies
 
     def run(self):
         self.process_1()
@@ -27,17 +49,21 @@ class gsxt(object):
         self.process_3()
         self.process_4()
         self.process_5()
+        # self.process_6()
+        self.process_7()
     
 
     def repeat(self, url, hd, ck={}):
         times = 5
         while times > 0:
             try:
-                ans = self.ssn.get(url, headers=hd, cookies=ck, timeout=5)
+                ans = self.ssn.get(url, headers=hd, cookies=ck, timeout=5, proxies=self.proxies)
+                # ans = self.ssn.get(url, headers=hd, cookies=ck, timeout=5)
+                # prox = requests.get("http://proxy.abuyun.com/current-ip", proxies=self.proxies)
+                # print(prox.text)
                 if ans.content: return ans
             except:
                 times -= 1
-
 
     def gen_timestamp(self):
         return str(int(random.random()*10000+int(time.time()*1000)))
@@ -54,59 +80,65 @@ class gsxt(object):
 
 
     def process_1(self):
+        # print('process_1 is started')
+        # prox = requests.get("http://proxy.abuyun.com/switch-ip", proxies=self.proxies)
         hd = {"Host": "www.gsxt.gov.cn",
               "User-Agent": self.user_agent}
         ans = self.repeat("http://www.gsxt.gov.cn/index.html", hd)
         self.ck_gsxt.update(self.merge_cookies(ans.cookies))
-                
+        # print('process_1 is complete')
 
     def process_2(self):
+        # print('process_2 is started')
         hd = {"Host": "www.gsxt.gov.cn",
               "Referer": "http://www.gsxt.gov.cn/index.html",
               "User-Agent": self.user_agent}
         ans = self.repeat("http://www.gsxt.gov.cn/SearchItemCaptcha?v={}".format(str(int(time.time()*1000))), hd, self.ck_gsxt)
         self.ck_gsxt.update(self.merge_cookies(ans.cookies))
         self.info.update(json.loads(ans.content))
-
+        # print('process_2 is complete')
 
     def process_3(self):
         hd = {"Host": "api.geetest.com",
               "Referer": "http://www.gsxt.gov.cn/index.html",
               "User-Agent": self.user_agent}
-        url = "http://api.geetest.com/gettype.php?gt={}&callback=geetest_{}".format(self.info["gt"], self.gen_timestamp())
-        ans = self.repeat(url, hd)
+        self.timestamp = self.gen_timestamp()
+        url = "http://api.geetest.com/gettype.php?gt={}&callback=geetest_{}".format(self.info["gt"], self.timestamp)
+        ans = self.repeat(url, hd, self.ck_geetest)
         self.ck_geetest.update(self.merge_cookies(ans.cookies))
-
+        # print('process_3 is complete')
 
     def process_4(self):
         hd = {"Host": "api.geetest.com",
               "Referer": "http://www.gsxt.gov.cn/index.html",
               "User-Agent": self.user_agent}
         url = "http://api.geetest.com/get.php?gt={}&challenge={}&product=popup&offline=false&protocol=&path=/static/js/geetest.5.10.10.js&type=slide&callback=geetest_{}".format(self.info["gt"], self.info["challenge"], self.gen_timestamp())
+
         ans = self.repeat(url, hd, self.ck_geetest)
+        # print('process 4 answer received')
         tjson = re.findall("\((.*?)\)", ans.content)[0]
         self.info.update(json.loads(tjson))
-
+        # print(self.info)
 
     def process_5(self):
         times = 4
         while True:
             xpos, tracks = self.get_xpos_trace()
-            print xpos
-            act = self.gee_f(self.gee_c(tracks))
-            time.sleep(0.6)
+            act = urllib.quote(self.gee_f(self.gee_c(tracks)).encode('utf8'), safe="~()*!.\'")
+            time.sleep(0.9)
             passtime = str(tracks[-1][-1])
-            imgload = str(random.randint(0,200) + 50)
+            imgload = str(random.randint(10,30))
             userresponse = self.gee_userresponse(xpos, self.info["challenge"])
-            time.sleep(0.6)
+            time.sleep(1.2)
             hd = {"Host": "api.geetest.com",
                   "Referer": "http://www.gsxt.gov.cn/index.html",
                   "User-Agent": self.user_agent}
             url = "https://api.geetest.com/ajax.php?gt={}&challenge={}&userresponse={}&passtime={}&imgload={}&a={}&callback=geetest_{}".format(self.info["gt"], self.info["challenge"], userresponse, passtime, imgload, act, self.gen_timestamp())
             ans = self.repeat(url, hd, self.ck_geetest)
+            print('process_5 result')
+            print(ans.content)
             self.ck_geetest.update(self.merge_cookies(ans.cookies))
             tjson = json.loads(re.findall("\((.*?)\)", ans.content)[0])
-            print ans.content
             if tjson["success"] != 1:
                 if times == 0:
                     raise NameError
@@ -115,16 +147,46 @@ class gsxt(object):
                 self.refresh()
                 times -= 1
                 continue
-
             else:
                 self.info["validate"] = tjson["validate"]
-                break   
+                break
+
+    def process_7(self):
+        print('process 7 started')
+        hd = {"Host": "www.gsxt.gov.cn",
+              "Connection": "keep-alive",
+              "Content-Length": "217",
+              "Cache-Control": "max-age=0",
+              "Origin": "http://www.gsxt.gov.cn",
+              "Upgrade-Insecure-Requests": "1",
+              "User-Agent": self.user_agent,
+              "Content-Type": "application/x-www-form-urlencoded",
+              "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+              "Referer": "http://www.gsxt.gov.cn/corp-query-search-1.html"}
+
+        print(self.ck_gsxt)
+
+        fl = {
+              "searchword": urllib.quote(self.company),
+              "geetest_challenge": self.info["challenge"],
+              "geetest_validate": self.info["validate"],
+              "geetest_seccode": urllib.quote(self.info["validate"]+"|jordan")}
+
+        url = "http://www.gsxt.gov.cn/corp-query-search-1.html"
+        # ans = self.ssn.post(url, data=fl, headers=hd,  proxies=self.proxies, timeout=10)
+        ans = self.ssn.post(url, data=fl, headers=hd, cookies=self.ck_gsxt, timeout=10)
+        # ans = scrapy.FormRequest(url, method='POST',formdata=fl, cookies=self.ck_gsxt, proxies=self.proxies)
+        print(ans.status_code)
+        self.parse_company_content(ans.content)
 
 
     def parse_company_content(self, sb):
         soup = BeautifulSoup(sb, 'html.parser')
-        for sp in soup.find_all("a", attrs={"class": "search_list_item"}):
-            print re.sub("\s", " ", sp.get_text().encode("utf-8"))
+        print(soup)
+        # for sp in soup.find_all("a", attrs={"class": "search_list_item db"}):
+        # for sp in soup.find_all("div", attrs={"class": "main-layout fw f14"}):
+            # print(re.sub("\s", " ", sp.get_text().encode("utf-8")))
+        print('parsing ended')
 
 
     def translate_array(self, tmp):
@@ -270,11 +332,13 @@ class crack_picture(object):
                 times -= 1   
 
 
+    ## find the xpos position where the images start to be different, but is 6 the starting point of every slide?
     def pictures_recover(self):
         xpos = self.judge(self.picture_recover(self.img1, 'img1.jpg'), self.picture_recover(self.img2, 'img2.jpg')) - 6
         return xpos, self.darbra_track(xpos)
 
 
+    ## recover the downloaded image to a visually correct format and return the recovered image
     def picture_recover(self, img, name):
         a =[39, 38, 48, 49, 41, 40, 46, 47, 35, 34, 50, 51, 33, 32, 28, 29, 27, 26, 36, 37, 31, 30, 44, 45, 43, 42, 12, 13, 23, 22, 14, 15, 21, 20, 8, 9, 25, 24, 6, 7, 3, 2, 0, 1, 11, 10, 4, 5, 19, 18, 16, 17]
         im = Image.open(img)
@@ -293,24 +357,57 @@ class crack_picture(object):
 
 
     def darbra_track(self, distance):
-        return [[distance, 0.5, 2134]]
-        #crucial trace code was deleted
-         
+        # to do: time has to be ascending, clientY has to be continuous
+        tracks = [[random.randint(-20,-10),random.randint(-20,-10),0],[0,0,0],[1,0,random.randint(150,300)]]
+        n_item = random.randint(15,100)
+        def dim_xpos_1(i, n, xpos):
+            x = (1-(i*1.0/n-1)**2)*xpos+1
+            # print(i)
+            # print(n)
+            # print(i*1.0/n)
+            # print(i*1.0/n-1)**2
+            return int(x)
 
+        def dim_xpos_2(i, n, xpos):
+            x = xpos/math.log(n)*math.log(i)
+            return int(x)
+
+        def dim_xpos_3(i):
+            x = random.randint(5,50)*(5*i)/(1+i**2)
+            return int(x)
+
+        def dim_xpos_4(i, n, xpos):
+            v = ((xpos - 0.1)/n**2)*(i**2)+0.1
+            return v
+
+        def dim_xpos_5(i, n, xpos):
+            v = 3*xpos*n^(-3)*(i-n)**2
+            return v
+
+        for i in range(2, n_item+1):
+            tracks.append([dim_xpos_1(i,n_item,distance), random.randint(-1, 1)+tracks[i][1], random.randint(10,20)+tracks[i][2]])
+        # tracks = [[-28,-20,0],[0,0,0],[2,0,281],[7,0,296],[14,0,313],[24,-1,330],[40,-3,348],[58,-4,365],[71,-5,383],[98,-7,400],[114,-9,416],[125,-10,434],[135,-10,451],[140,-10,468],[142,-10,486],[144,-10,506],[145,-10,522],[145,-10,592]]
+        # print('array length is {}'.format(n_item))
+        # print('xpos is {}'.format(distance))
+        # print('passtime is {}'.format(tracks[-1][-1]))
+        # print('tracks is {}'.format(tracks))
+        return tracks
+         
+    ## function to evaluate pixel difference
     def diff(self, img1, img2, wd, ht):
         rgb1 = img1.getpixel((wd, ht))
         rgb2 = img2.getpixel((wd, ht))
         tmp = reduce(lambda x,y: x+y, map(lambda x: abs(x[0]-x[1]), zip(rgb1, rgb2)))
         return True if tmp >= 190 else False
 
-            
+    ## check if the image pixel point is the same or not, return true if is different
     def col(self, img1, img2, cl):
         for i in range(img2.size[1]):
             if self.diff(img1, img2, cl, i):
                 return True
         return False
 
-
+    ## return the pixel column number where the two images are different
     def judge(self, img1, img2):
         for i in range(img2.size[0]):
             if self.col(img1, img2, i):
@@ -319,7 +416,8 @@ class crack_picture(object):
 
 
 if __name__ == "__main__":
-    for company in ["招商银行", '交通银行', '建设银行']:        
+    for company in ['小米', '交通银行', '建设银行']:
+
         jsh = gsxt(company)
         jsh.run()         
         time.sleep(2)
